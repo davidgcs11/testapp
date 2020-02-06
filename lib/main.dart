26c6c59142987f7ab294df03fbf6f06d7d3f7880
olivergarciadp <54827647+olivergarciadp@users.noreply.github.com>
2020-02-06 10:48:48 -0500

    First commit, Flutterapp just created

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatApp(),
+    );
+  }
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(),
+          TextFormField(),
+        ],
+      ),
+    );
+  }
+}
